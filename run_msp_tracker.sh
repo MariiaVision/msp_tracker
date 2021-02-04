b168d074e94544212d631d0eb880656cb0a8c12c
MariiaVision <mari.dmitrieva@gmail.com>
2021-02-04 16:03:17 +0000

    descriptions are added

--- run_msp_tracker.sh
+++ run_msp_tracker.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-source activate gui_tracker
+conda activate gui_tracker
+
+################## SET PATHS BELOW: 
+
+# path to the image sequence (the file should be tiff format, single channel)
+MOVIE_PATH = "/path/to/the/tif/file"
+
+# path to the file with detection parameters (created with the MSP-tracker GUI)
+DETECTION_PARAMETERS_PATH = "/path/to/the/parameter/file"
+
+#path to the file with linking parameters (created with the MSP-tracker GUI)
+LINKING_PARAMETERS_PATH = "/path/to/the/parameter/file"
+
+# True or False: set it to False to run the detection part of the tracker, and True to use existing detections from a file
+USE_EXIST_DETECTION = False
+
+# path to the file with the detection for the current image sequence. 
+# When USE_EXIST_DETECTION is False - detections will be saved there, otherwise (when True) - detections will be read from the file
+DETECTION_PATH = "/path/to/the/detection/file"
+
+# path where to save the trajectories and name of the file
+RESULT_PATH = "/path/to/the/file/with/results"
 
 
-MOVIE_PATH="/media/mariaa/tracking/test_data/2018_06_12-50min_bio_10fps_AiryscanProcessing-f150-200.tif"
-
-DETECTION_PARAMETERS_PATH="/media/mariaa/tracking/test_data/2018_06_12-50min_bio_10fps_AiryscanProcessing-f150-200_detection_set.txt"
-
-LINKING_PARAMETERS_PATH="/media/mariaa/tracking/test_data/2018_06_12-50min_bio_10fps_AiryscanProcessing-f150-200_linking_set.txt"
-
-USE_EXIST_DETECTION="True"
-
-DETECTION_PATH="/media/mariaa/tracking/test_data/2018_06_12-50min_bio_10fps_AiryscanProcessing-f150-200_detection.txt"
-
-RESULT_PATH="/media/mariaa/tracking/test_data/2018_06_12-50min_bio_10fps_AiryscanProcessing-f150-200_tracks_1.txt"
-
-
-
+# running the code
 python tracking_lib/tracking_pipeline.py --movie_path $MOVIE_PATH --detection_parameters_path $DETECTION_PARAMETERS_PATH  --linking_parameters_path $LINKING_PARAMETERS_PATH --use_existing_detection $USE_EXIST_DETECTION --detection_path $DETECTION_PATH --result_path $RESULT_PATH
